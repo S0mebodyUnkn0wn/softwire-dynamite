c01d35282244a9163a8705d41df34684f3250949
Daniel Guryev <Daniel.Guryev@gmail.com>
2025-07-02 17:26:30 +0100

    Add a test bench script

---
+++ test.py
@@ -0,0 +1,36 @@
+
+
+import sys
+import os
+
+
+
+
+if __name__ == "__main__":
+
+    os.system("rm out.txt")
+
+    bot1 = "smartBotMk2.js"
+    bot2 = "smartBotMk3.js"
+
+    command = f"node dynamite-cli.js {bot1} {bot2} >> out.txt"
+    for i in range(200):
+        os.system(command)
+    
+
+    with open("out.txt", "r") as f:
+        text = f.read()
+        lines = text.split("\n")
+        total_diff=0
+        c=0
+        for line in lines:
+            if line.startswith("Score"):
+                c+=1
+                total_diff += eval (line[7:])
+
+        
+        print("avg difference", total_diff/c)
+        print(bot1, text.count("p1"))
+        print(bot2, text.count("p2"))
+
+
